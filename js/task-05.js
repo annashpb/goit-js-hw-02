12d71afd6883c9d5bb7ef158294562126727a5df
Anna Sh <anna.sh.pb@gmail.com>
2019-09-15 14:44:35 +0300

    task05

---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function checkForSpam(str) {
+    let string = str.toLowerCase();
+    return (string.includes('spam') || string.includes('sale'));
+}
+
+console.log(checkForSpam('Latest technology news'));
+console.log(checkForSpam('JavaScript weekly newsletter'));
+console.log(checkForSpam('Get best sale offers now!'));
+console.log(checkForSpam('[SPAM] How to earn fast money?'));
